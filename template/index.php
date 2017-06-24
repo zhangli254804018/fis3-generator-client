9049290171f3d334dfee991cdd93656cdad1d107
章魚哥 <254804018@qq.com>
2017-06-24 16:27:24 +0800

    Update package.json

--- template/index.php
+++ template/index.php
@@ -1,5 +1,37 @@
 <?php
-    include "config.php";
+  $v = "1.10";
+
+  $debug = isset($_GET["debug"]);
+  //$debug = true;
+
+  $defaultUser = $debug ? "fion0210" : "";
+  $user = isset($_GET["u"]) ? $_GET["u"] : $defaultUser;
+  $source = isset($_GET["source"]) ? $_GET["source"] : "";
+  $uid = isset($_SESSION['uid']) ? $_SESSION['uid'] : 0;
+
+  $apiLogo = "http://api2.raidtalk.com.tw/server/getimg.php?sid=%s&type=100";
+  $config = array(
+      "debug" => $debug,
+      "user" => $user,
+      "uid" => $uid,
+      "apiLogo" =>$apiLogo,
+      "apis" => array(
+          "show" => $rc_showhome,
+          "game" => $rc_game,
+          "team" => $rc_team,
+          "live" =>$rc_live
+      ),
+      "langCode" => "zh-tw",
+      "areas" => array(
+          "zh-tw" => array("保密不顯示", "澳大利亞", "中國大陸", "加拿大", "美國", "馬來西亞", "日本", "台灣", "香港", "新加坡", "英國"),
+          "zh-cn" => array("保密不显示", "澳大利亞", "中国大陆", "加拿大", "美国", "马来西亚", "日本", "台湾", "香港", "新加坡", "英国")
+      ),
+      "appFiles" => array(
+          "android" => "//rcshow.tv/apps/download/getApp.php?type=android&source=$source",
+          "ios" => "//rcshow.tv/apps/download/getApp.php?type=ios&source=$source",
+          "pc" => "//rcshow.tv/apps/download/"
+      )
+  );
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,97 +49,90 @@
     <?php }?>
 </head>
 
-<body>
-    <section class="page-header">
-        <h1 class="project-name">fis3-client</h1>
-        <h2 class="project-tagline">Mr. Zhang, Packaged web front-end architecture for PC terminals and so on. full vesion.</h2>
+ <body>
+    <header>
+      <div class="inner">
+        <a href="https://zhangli254804018.github.io/fis3-generator-client/">
+          <h1>fis3-generator-client</h1>
+        </a>
+        <h2>Create a fis3 project</h2>
+        
+          <a href="https://github.com/zhangli254804018/fis3-generator-client" class="button"><small>View project on</small> GitHub</a>
+        
+        
+      </div>
+    </header>
 
-        <a href="https://github.com/zhangli254804018/fis3-client" class="btn">View on GitHub</a>
+    <div id="content-wrapper">
+      <div class="inner clearfix">
+        <section id="main-content">
+          <h1 id="fis3-generator-client">fis3-generator-client</h1>
+<p>Create a fis3 project</p>
 
+<ul>
+  <li>1、使用項目結構為</li>
+</ul>
 
-    </section>
-
-    <section class="main-content">
-        <p>#fis3-client</p>
-
-        <ul>
-            <li>1、项目采用fis3开发结构项目</li>
-            <li>2、支持common.js写法等 不依赖requirejs等</li>
-            <li>3、支持es6语法等</li>
-            <li>4、可以实行单页面和多页面的开发 要修改入口配置</li>
-            <li>
-                <p>5、原则上fis不针对文件做修改 编辑等</p>
-
-                <p>但是为了启动单页面 更好的开发 我们需要在项目上做以下修改来适应开发。 a.新增解析hook-commonjs &amp; hook-amd 等插件兼容写法模块 b.新增解析parser-less 插件解析less文本文件</p>
-            </li>
-        </ul>
-
-        <div class="highlighter-rouge"><pre class="highlight"><code>        "fis-parser-less": "^0.1.3",
-        "fis-parser-utc": "^0.0.2",
-        "fis3-hook-amd": "^0.2.0",
-        "fis3-hook-commonjs": "^0.1.26",
-        "fis3-hook-node_modules": "^2.2.8",
-        "fis3-parser-client": "*",
-        "fis3-postpackager-autoloader": "^1.0.2",
-        "fis3-postpackager-loader": "^2.1.4",
-        "fis3-postpackager-simple": "^0.0.27"
+<div class="highlighter-rouge"><pre class="highlight"><code>        開發過程第一步為
+        npm install fis3-generator-client -g
+        第二步創建文件夾名稱
+        fisc demoApp
 </code></pre>
-        </div>
-        <ul>
-            <li>6、使用項目結構為
-                <div class="highlighter-rouge"><pre class="highlight"><code>      依賴包：fis3-client
-      參考文件結構為:
-              -assets
-                      -img
-                      -less
-                      -css
-                      -pic
-                      -swf
-                      ……
-              js
-                      -api
-                      -dist
-                      -dev
-                      index.js
-                      ……
-              lib
-                      -jquery
-                      ……
-              fis-conf.js
-              package.json
-                      "fis3-client":"*"
+</div>
+<p>生成文件結構為</p>
+
+<div class="highlighter-rouge"><pre class="highlight"><code>        依賴包：fis3-client
+        參考文件結構為:
+                -assets
+                        -img
+                        -css
+                        ……
+                js
+                        index.js
+                        ……
+                lib
+                        ……
+                fis-conf.js
+                package.json
+                        "fis3-client":"*"
 </code></pre>
-                </div>
-            </li>
-            <li>7、后期需要更多的优化兼容开发体验等</li>
-        </ul>
+</div>
 
-        <div class="highlighter-rouge"><pre class="highlight"><code>        開發過程第一步為
-        npm run dev
-        開發訪問地址：your host+/dev?debug=1
-        正式環境為
-        npm run build
-        正式訪問地址：your host+/dist
+<p>但是为了启动单页面 更好的开发 
+   我们需要在项目上做以下修改来适应开发。</p>
+
+<div class="highlighter-rouge"><pre class="highlight"><code>        "commander": "2.7.1",
+        "mkdirp": "0.5.1",
+        "sorted-object": "1.0.0"
 </code></pre>
-        </div>
+</div>
 
-        <ul>
-            <li><a href="https://github.com/zhangli254804018/fis3-client">架构源码参考</a></li>
-            <li><a href="https://www.npmjs.com/package/fis3-parser-client">核心依賴插件-fis3-parser-client</a></li>
-        </ul>
-
-
-
-        <footer class="site-footer">
-
-            <span class="site-footer-owner"><a href="https://github.com/zhangli254804018/fis3-client">fis3-client</a> is maintained by <a href="https://github.com/zhangli254804018">zhangli254804018</a>.</span>
-
-            <span class="site-footer-credits">This page was generated by <a href="https://pages.github.com/">GitHub Pages</a>.</span>
-        </footer>
-    </section>
+<ul>
+  <li>
+    <p>2、后期需要更多的优化兼容开发体验等</p>
+  </li>
+  <li><a href="https://www.npmjs.com/package/fis3-client">架构源码参考</a></li>
+  <li><a href="https://www.npmjs.com/package/fis3-parser-client">核心依賴插件-fis3-parser-client</a></li>
+  <li><a href="http://vc.changyou.com/">技術支持</a></li>
+</ul>
 
 
+        </section>
 
+        <aside id="sidebar">
+          
+
+          
+            <p class="repo-owner"><a href="https://github.com/zhangli254804018/fis3-generator-client">fis3-generator-client</a> is maintained by <a href="https://github.com/zhangli254804018">zhangli254804018</a>.</p>
+          
+
+          <p>This page was generated by <a href="https://pages.github.com/">GitHub Pages</a>.</p>
+        </aside>
+      </div>
+    </div>
+
+    
+  
 
 </body>
 <?php if ($debug) {?>
--- template/js/main.js
+++ template/js/main.js
@@ -1,27 +1,2 @@
 // 入口文件等
 //const _ =  require('underscore');
-function getIframeWindow(obj) {
-    return obj.contentWindow || obj.contentDocument.parentWindow;
-}
-
-function getIframeHeight(obj) {
-    var idoc = getIframeWindow(obj).document;
-    var ibody = document.getElementsByTagName('html')[0];
-    if (idoc.body) {
-        return {
-            height: Math.max(idoc.body.scrollHeight, idoc.body.offsetHeight, idoc.body.clientHeight, ibody.clientHeight)
-        }
-
-    } else if (idoc.documentElement) {
-        return {
-            height: Math.max(idoc.documentElement.scrollHeight, idoc.documentElement.offsetHeight, ibody.clientHeight)
-        }
-    }
-}
-
-function setHeight() {
-    var myiframe = document.getElementById("myiframe");
-    myiframe.height = getIframeHeight(myiframe)['height'];
-}
-
-setHeight()
